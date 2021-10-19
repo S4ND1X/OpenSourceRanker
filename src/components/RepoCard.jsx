08c91f4c565439bed77df14331d91a8112eec538
Sebastian Crossa <sebastiancrossa@hotmail.com>
2021-10-19 17:39:16 -0500

    feat: fetch from the Github API

--- src/components/RepoCard.jsx
+++ src/components/RepoCard.jsx
@@ -1,6 +1,28 @@
-import React from "react";
+import { Octokit } from "@octokit/core";
+import { useState, useEffect } from "react";
 
-const RepoCard = () => {
+const octokit = new Octokit({ auth: process.env.GITHUB_API_KEY });
+
+const RepoCard = ({ repo }) => {
+  const [repoInfo] = useState(repo.split("/").slice(1));
+
+  useEffect(() => {
+    console.log(repoInfo);
+
+    const fetchData = async () => {
+      const res = await octokit
+        .request(`GET /repos/{owner}/{repo}`, {
+          owner: repoInfo[0],
+          repo: repoInfo[1],
+        })
+        .then((res) => res.data);
+
+      console.log(res);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div>
       <h2>This is a repo card</h2>
