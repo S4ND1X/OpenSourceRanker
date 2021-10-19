0911e15a79264003bc8336716442ea2a50aef5de
Sebastian Crossa <sebastiancrossa@hotmail.com>
2021-10-19 15:08:25 -0500

    chore: prep for repos list component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
+// Libraries
 import React from "react";
 import { Container } from "./styles";
+
+// Component imports
 import Navbar from "./components/layout/Navbar";
+import ReposList from "./components/containers/ReposList";
 
 function App() {
   return (
     <>
       <Navbar />
       <Container>
-        <div>
-          <p>Cleanedup React App</p>
-        </div>
+        <ReposList />
       </Container>
     </>
   );
