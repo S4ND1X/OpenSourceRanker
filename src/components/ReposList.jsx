7fff561e5a96d19a4637586cbd1970fe37691e8c
Sebastian Crossa <sebastiancrossa@hotmail.com>
2021-10-19 18:33:11 -0500

    feat: style repo card

--- src/components/ReposList.jsx
+++ src/components/ReposList.jsx
@@ -1,17 +1,32 @@
 import React from "react";
+import styled from "styled-components";
 import { repos } from "../lib/helpers";
 import RepoCard from "./RepoCard";
 
 const ReposList = () => {
   return (
-    <div>
+    <Background>
       <h2>Top 10 most popular repos</h2>
 
-      {repos.map((repo) => (
-        <RepoCard repo={repo} />
-      ))}
-    </div>
+      <div className="list">
+        {repos.map((repo) => (
+          <RepoCard repo={repo} />
+        ))}
+      </div>
+    </Background>
   );
 };
+
+const Background = styled.div`
+  .list {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-gap: 1rem;
+  }
+
+  h2 {
+    margin-bottom: 1rem;
+  }
+`;
 
 export default ReposList;
