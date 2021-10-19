eb55d33d7abb9bd2d8034731eddd373c3238f3e5
Sebastian Crossa <sebastiancrossa@hotmail.com>
2021-10-19 17:39:16 -0500

    feat: fetch from the Github API

--- src/components/ReposList.jsx
+++ src/components/ReposList.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { repos } from "../lib/helpers";
+import RepoCard from "./RepoCard";
 
 const ReposList = () => {
   return (
     <div>
       <h2>Top 10 most popular repos</h2>
-      {/* repos go here */}
+
+      {repos.map((repo) => (
+        <RepoCard repo={repo} />
+      ))}
     </div>
   );
 };
---
+++ src/lib/helpers.js
@@ -0,0 +1,12 @@
+export const repos = [
+  "/maticzav/nookies",
+  "/danielsneijers/react-flash-message",
+  "/Bunlong/react-papaparse",
+  "/dcampos/nvim-snippy",
+  "/sebastiancrossa/root-visualizer",
+  "/sebastiancrossa/tradedomainswithme",
+  "/sebastiancrossa/visionapp",
+  "/MLH-Fellowship/spotify-web-app",
+  "/S4ND1X/AlgorithmicClubGDAFullstack",
+  "/S4ND1X/RickAndMortyJS",
+];
