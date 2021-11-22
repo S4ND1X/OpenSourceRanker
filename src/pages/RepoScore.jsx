2f0f00cd3898ca77ee45fc42f205539b04e497b6
Sebastian Crossa <sebastiancrossa@hotmail.com>
2021-11-22 12:02:08 -0600

    hotfix: repo vars not pulled correctly

--- src/pages/RepoScore.jsx
+++ src/pages/RepoScore.jsx
@@ -1,11 +1,9 @@
 import { useParams } from "react-router";
+import { octokit } from "../lib/octokit";
 import Footer from "../components/layout/Footer";
 import Navbar from "../components/layout/Navbar";
 import { Container } from "../styles";
-import { Octokit } from "@octokit/core";
 import { useEffect } from "react";
-
-const octokit = new Octokit({ auth: process.env.REACT_APP_GITHUB_API_KEY });
 
 const RepoScore = () => {
   const { url: encodedUrl } = useParams();
@@ -14,19 +12,17 @@ const RepoScore = () => {
   useEffect(() => {
     const fetchRepoData = async () => {
       // Get the repo owner and name from the url
-      const repoOwner = url.split("/").slice(1)[3];
-      const repoName = url.split("/").slice(1)[4];
-      console.log(process.env.REACT_APP_GITHUB_API_KEY);
+      const repoOwner = url.split("/").slice(1)[2];
+      const repoName = url.split("/").slice(1)[3];
 
-      await octokit
-        .request(`GET /repos/{owner}/{repo}`, {
-          owner: repoOwner,
-          repo: repoName,
-        })
-        .then((res) => {
-          console.log(res);
-        });
+      const { data } = await octokit.request("GET /repos/{owner}/{repo}", {
+        owner: repoOwner,
+        repo: repoName,
+      });
+
+      console.log(data);
     };
+
     fetchRepoData();
   }, [url]);
 
@@ -36,7 +32,6 @@ const RepoScore = () => {
       <Container>
         <h1>Repo score page for:</h1>
         <p>{url}</p>
-        
       </Container>
       <Footer />
     </>
